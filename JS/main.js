5d2b4a343cbfba645cd69d301ae0f280c8101d94
Raul <raul.bethencourt.pro@gmail.com>
2020-11-05 22:21:03 +0100

    first exos

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+// console.log('test');
+// console.log('hello World !');
+let a = 3;
+let b;
+
+var myStr = `'frase de prueba "vamos a ver" el resultado'`;
+a++;
+
+var array = [
+    ["test", 1],
+    ["prueba", 2],
+];
+
+array.push(['mas', 2])
+
+console.log(array);
+console.log(a);
