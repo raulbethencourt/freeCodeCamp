dcc8002103bae81e1224db72856671f2ad7b7e2f
Raul <raul.bethencourt.pro@gmail.com>
2020-12-31 15:49:45 +0100

    :sparkles: finish counter challenge

---
+++ javascript-basic-projects-master/2-counter/setup/app.js
@@ -0,0 +1,39 @@
+// Launch app
+document.addEventListener("readystatechange", (evt) => {
+	if (evt.target.readyState === "complete") {
+		initApp();
+	}
+});
+
+const initApp = () => {
+	const valueCase = document.getElementById("value");
+	const buttons = document.querySelectorAll(".btn");
+	let value = 0;
+	valueCase.innerText = value;
+
+	buttons.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			switch (btn.getAttribute("data-target")) {
+				case "decrease":
+					value--;
+					break;
+				case "increase":
+					value++;
+					break;
+				default:
+					value = 0;
+					break;
+			}
+
+			if (value < 0) {
+				valueCase.style.color = "red";
+			} else if (value > 0) {
+				valueCase.style.color = "green";
+			} else {
+				valueCase.style.color = "hsl(209, 61%, 16%)";
+			}
+			
+			valueCase.innerText = value;
+		});
+	});
+};
